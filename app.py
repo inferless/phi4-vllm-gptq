84f4886c98d49461b0fa4139afd1da46171dffee
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-03-05 11:13:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ class InferlessPythonModel:
     def infer(self, inputs):
         prompt = inputs["prompt"]
         system_prompt = inputs.get("system_prompt","You are a friendly bot.")
-        temperature = inputs.get("temperature",1.0)
-        top_p = inputs.get("top_p",1.0)
-        top_k = int(inputs.get("top_k",-1))
-        max_tokens = inputs.get("max_tokens",256)
+        temperature = inputs.get("temperature",0.1)
+        top_p = inputs.get("top_p",0.1)
+        top_k = int(inputs.get("top_k",40))
+        max_tokens = inputs.get("max_tokens",128)
         
         sampling_params = SamplingParams(temperature=temperature,top_p=top_p,
                                          top_k=top_k,max_tokens=max_tokens
