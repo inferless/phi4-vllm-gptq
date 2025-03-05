84381707efdc8ff9fe41427159c5ef3e7b02f37f
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-03-05 11:50:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
-from huggingface_hub import snapshot_download
 from vllm import LLM, SamplingParams
 
 class InferlessPythonModel:
-    def initialize(self):        
-        model_id = "unsloth/phi-4-GGUF"
-        model_path = snapshot_download(repo_id=model_id,allow_patterns=["phi-4-Q4_K_M.gguf"])
-        self.llm = LLM(model=f"{model_path}/phi-4-Q4_K_M.gguf",tokenizer="microsoft/phi-4")
-        
+    def initialize(self):
+        self.llm = LLM(model="kaitchup/Phi-4-AutoRound-GPTQ-4bit", quantization="gptq")
+
     def infer(self, inputs):
         prompt = inputs["prompt"]
         system_prompt = inputs.get("system_prompt","You are a friendly bot.")
-        temperature = inputs.get("temperature",0.1)
-        top_p = inputs.get("top_p",0.1)
-        top_k = int(inputs.get("top_k",40))
-        max_tokens = inputs.get("max_tokens",128)
+        temperature = inputs.get("temperature",1.0)
+        top_p = inputs.get("top_p",1.0)
+        top_k = int(inputs.get("top_k",-1))
+        max_tokens = inputs.get("max_tokens",256)
         
         sampling_params = SamplingParams(temperature=temperature,top_p=top_p,
                                          top_k=top_k,max_tokens=max_tokens
